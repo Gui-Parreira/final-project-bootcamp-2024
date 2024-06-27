35eed12577cba0d69bd87e2879629faa0870a8f3
Gui-Parreira <gui.parreira98@gmail.com>
2024-06-27 20:14:55 -0300

    Criação perfil de produção e integração com Railway

--- src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -1,12 +1,10 @@
 package me.dio.controller.exception;
 
-import me.dio.domain.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.rmi.UnexpectedException;
